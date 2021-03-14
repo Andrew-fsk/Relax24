3dde8616edac5689574e17495eab4dd68b45fc51
Andrew <feshykandrew.work@gmail.com>
2021-03-14 17:37:23 +0200

    form and success modal

--- js/main.js
+++ js/main.js
@@ -215,6 +215,21 @@ $(document).ready(function () {
     }).each(function () {
         $(this).val() ? $(this).parent().addClass('active') : $(this).parent().removeClass('active');
     });
+
+
+    $('.js-popup__close').click(close_popup);
+
+    $('.js-btn-popup').click(function (e) {
+        e.preventDefault();
+        let index_btn_popup = $(this).attr('href');
+        openPopup(index_btn_popup);
+    });
+
+    $('.js-popup').click(function (e) {
+        let popup = $('.js-popup__wrapp');
+        if (!popup.is(e.target) && popup.has(e.target).length === 0)
+            close_popup();
+    });
 })
 
 function setPopup(){
@@ -227,3 +242,11 @@ function setPopup(){
 $(window).resize(function () {
     setPopup();
 });
+
+function openPopup(id) {
+    $(".js-popup[data-id-popup='" + id + "']").fadeIn(300);
+}
+
+function close_popup() {
+    $('.js-popup').fadeOut(300);
+}
